64971d444ff01ff6ae8aa85d1764adc00e64f50b
Veralynia <mandybesse@gmail.com>
2026-03-23 22:57:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
-});
-
-const token = process.env.DISCORD_TOKEN;
-
-if (!token) {
-    console.log("Kein DISCORD_TOKEN gefunden!");
-    process.exit(1);
-}
-
-client.once('ready', () => {
-    console.log(`Eingeloggt als ${client.user.tag}`);
-});
-
-client.login(token);
+console.log("DISCORD_TOKEN vorhanden:", !!process.env.DISCORD_TOKEN);
